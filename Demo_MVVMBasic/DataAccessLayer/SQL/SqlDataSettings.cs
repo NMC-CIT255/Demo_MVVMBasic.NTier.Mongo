c577fd62a073795f7d8f1f5ab23463830460a67c
johnvelis <john@johnvelis.com>
2020-09-25 12:52:04 -0400

    updated connection string

--- Demo_MVVMBasic/DataAccessLayer/SQL/SqlDataSettings.cs
+++ Demo_MVVMBasic/DataAccessLayer/SQL/SqlDataSettings.cs
@@ -8,6 +8,7 @@ namespace Demo_MVVMBasic.DataAccessLayer
 {
     public static class SqlDataSettings
     {
-        public static string ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\velisj\Desktop\_Git\NMC\CIT255\_Active Solutions\Demo_MVVMBasic.NTier.Mongo\Demo_MVVMBasic\DataAccessLayer\SQL\Widgets.mdf"";Integrated Security=True";
+        // TODO (Demo Mode) - update the connection string
+        public static string ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\velisj\Desktop\_Git\NMC\CIT255\_Active Solutions\Demo_MVVMBasic.NTier.MongoSql\Demo_MVVMBasic\DataAccessLayer\SQL\Widgets.mdf"";Integrated Security=True";
     }
 }
